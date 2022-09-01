31dea60fd8214d099dbbbcd0d1267f0e546e18fe
Della Shelton <dellashelton@quickenloans.com>
2022-09-01 18:11:36 -0400

    added classes

---
+++ Generator/FileManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace QuestionGenerator
+{
+    public static class FileManager
+    {
+        public static string text = @"C:/Temp/Questions.txt";
+        public static List<string> SafeQuestions = new List<string>();
+        public static List<string> NsfwQuestions = new List<string>();
+        public static List<string> AllQuestions = new List<string>();
+
+        public static async void CheckFile()
+        {
+            if (!File.Exists(text))
+            {
+                await using FileStream fs = File.Create(text);
+            }
+            File.WriteAllText(text, "How did you get your name?,safe");
+        }
+
+        private static Dictionary<string,string> GetAllQuestions(string text)
+        {
+            CheckFile();
+
+            return File.ReadLines(text)
+                .Select(x => x.Split(','))
+                .ToDictionary(split => split[1],
+                split => split[0]);
+        }
+
+        public static void SeperateQuestions()
+        {
+            var questions = GetAllQuestions(text);
+            
+            foreach (var question in questions)
+            {                
+                AllQuestions.Add(question.Value);
+
+                if (question.Key == "safe")
+                {
+                    SafeQuestions.Add(question.Value);
+                }
+                else
+                {
+                    NsfwQuestions.Add(question.Value);
+                }
+            }
+        }
+
+        public static void AddQs()
+        {
+            Console.Clear();
+            Console.WriteLine($"Add question: ");
+            var newQuestion = Console.ReadLine();
+
+            using StreamWriter sw = File.AppendText(text);
+            sw.Write($"\n{newQuestion}");
+
+            SeperateQuestions();
+        }
+    }
+}
---
+++ Generator/Generator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestionGenerator
+{
+    public static class Generator
+    {
+        public static void GetQuestions()
+        {
+            var questions = GetQuestionList();
+
+            foreach (var line in questions)
+            {
+                Console.Clear();
+                Console.WriteLine($"{line} \n");
+
+                if (!Continue())
+                {
+                    break;
+                }
+            }
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine($"Next Question? (Y) Add Question (A)");
+            var response = char.ToLower(Console.ReadKey().KeyChar);
+
+            switch (response)
+            {
+                case 'y':
+                    return true;
+                case 'a':
+                    FileManager.AddQs();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static List<string> GetQuestionList()
+        {
+            FileManager.SeperateQuestions();
+
+            Console.WriteLine("What kind of questions do you want?\n1. All Questions\n2.Safe Questions\n3.Not Safe for For Questions");
+            int response;
+            var isNumber = int.TryParse(Console.ReadLine(), out response);
+
+            while (isNumber && response < 3 && response > 0)
+            {
+                Console.WriteLine("What kind of questions do you want?\n1. All Questions\n2.Safe Questions\n3.Not Safe for For Questions");
+            }
+
+            switch (response)
+            {
+                case 1:
+                    return FileManager.AllQuestions;
+                case 2:
+                    return FileManager.SafeQuestions;
+                case 3:
+                    return FileManager.NsfwQuestions;
+                default:
+                    return FileManager.AllQuestions;
+            }
+
+        }
+    }
+}
